e224e54dc144e4060b97c15d9dca7bda47cfca2b
jennlopezdv <jenniferlopezdavid71@gmail.com>
2026-03-11 18:02:51 -0500

    Matching copleto

--- src/modules/matching/matching.routes.js
+++ src/modules/matching/matching.routes.js
@@ -1,8 +1,8 @@
-/* import { Router } from "express";
+import { Router } from "express";
 import { runMatchingController } from "./matching.controller.js";
 
 const router = Router();
 
 router.get("/job-offers/:offerId/matches", runMatchingController);
 
-export default router; */
+export default router;
