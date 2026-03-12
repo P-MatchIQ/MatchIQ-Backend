c92ff4649aba8ec81ca776a85507d96bb84326ed
jennlopezdv <jenniferlopezdavid71@gmail.com>
2026-03-12 17:45:45 -0500

    middleware en matching

--- src/modules/matching/matching.routes.js
+++ src/modules/matching/matching.routes.js
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import { runMatchingController } from "./matching.controller.js";
+import { authenticate } from "../../middlewares/authenticate.js";
+import { authorize } from "../../middlewares/authorize.js";
 
 const router = Router();
 
-router.get("/job-offers/:offerId/matches", runMatchingController);
+router.get("/job-offers/:offerId/matches", authenticate, authorize("company", "admin"), runMatchingController);
 
 export default router;
