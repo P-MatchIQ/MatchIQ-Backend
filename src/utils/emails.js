34d34e7a8be6a6b12cf868d1121872166c56cf8a
carloscastano25 <carlos.castano@est.iudigital.edu.co>
2026-03-15 20:55:43 -0500

    Changing ipv6 to ipv4

--- src/utils/emails.js
+++ src/utils/emails.js
@@ -6,6 +6,7 @@ const transporter = nodemailer.createTransport({
     user: process.env.GMAIL_USER,
     pass: process.env.GMAIL_PASS,
   },
+  family: 4, 
 });
 
 export async function sendPasswordResetEmail({ to, resetUrl }) {
