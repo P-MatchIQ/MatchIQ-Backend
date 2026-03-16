2f9af675e1aa15a3dad6d51ddaa19d1155d2fa83
carloscastano25 <carlos.castano@est.iudigital.edu.co>
2026-03-15 19:24:25 -0500

    Cambio en server.js 0.0.0.0

--- src/server.js
+++ src/server.js
@@ -12,8 +12,8 @@ async function startServer() {
     console.log('Database connected');
 
     // Inicia servidor solo si la DB responde
-    app.listen(PORT, () => {
-      console.log(`Server running on port ${PORT}`);
+    app.listen(PORT, '0.0.0.0', () => {
+    console.log(`Server running on port ${PORT}`);
     });
 
   } catch (error) {
